24c302199d2701ce0f099f1c296461d1461466f6
jqsafi <shafayat@engineer.com>
2026-03-14 09:10:16 +0600

    ci: Add GitHub Action for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // For static hosting like GitHub Pages
+  base: '/prompt-gallery/', // Set base path for GitHub Pages
   build: {
     outDir: 'dist',
     sourcemap: true,
